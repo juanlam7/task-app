34aef7a3209cad04ce511e6e564556504f06a46b
Juan <juanlam7@gmail.com>
2022-01-25 16:39:35 -0400

    Monthly page styles and layout

--- src/pages/Monthly/index.js
+++ src/pages/Monthly/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import { Stats } from '../../components/Stats';
+import { ListMonth } from '../../components/ListMonths';
+
 function Monthly () {
 
 
-    return (
-        <div>hello</div>
-    )
+    return <>
+            <Stats />
+            <ListMonth />
+        </>
+    
 }
 
 export { Monthly }
